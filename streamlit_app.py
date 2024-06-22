a8f691806aef9af5e96729dda20bcc8ca3ee1f17
Bamore02 <128315193+Bamore02@users.noreply.github.com>
2024-06-22 23:48:15 +0200

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from hugchat import hugchat
 from hugchat.login import Login
-from base import hf_email, hf_pass
+
 from base import retrieval
 # App title
 st.set_page_config(page_title="🤗💬 HugChat")
